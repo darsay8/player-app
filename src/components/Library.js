32a4f374d2b00d98fde640db13497f0a8edf632c
darsay8 <darsay.one@gmail.com>
2022-03-10 00:23:46 -0300

    library added

---
+++ src/components/Library.js
@@ -0,0 +1,15 @@
+import LibrarySong from './LibrarySong';
+
+const Library = ({ songs }) => {
+  return (
+    <div className="library">
+      <div className="library-songs">
+        {songs?.map(song => (
+          <LibrarySong song={song} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Library;
